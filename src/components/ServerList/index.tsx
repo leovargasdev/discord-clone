0e528a621df29f6d2c69a186c018bc44188cc77d
Leo Vargas <leu1607@hotmail.com>
2020-06-30 14:27:40 -0300

    Adicionando icones aos servidores

--- src/components/ServerList/index.tsx
+++ src/components/ServerList/index.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
 
 import { Container, Separator, Server } from './styles';
-import Logo from '../../assets/server-logo.jpg';
+import Discord from '../../assets/icons/discord.svg';
+import Github from '../../assets/icons/github.svg';
+import Instagram from '../../assets/icons/instagram.svg';
+import Rocket from '../../assets/icons/rocket.svg';
+import Linkedin from '../../assets/icons/linkedin.svg';
+import Spotify from '../../assets/icons/spotify.svg';
 
 const ServerList: React.FC = () => (
   <Container>
     <Server isHome>
-      <img src={Logo} alt="Logo Fake Server Discord" />
+      <img src={Discord} alt="Logo Fake Server Discord" />
     </Server>
 
     <Separator />
 
-    <Server />
-    <Server />
+    <Server
+      href="https://github.com/LeeonardoVargas"
+      target="_new"
+      hasNotifications
+    >
+      <img src={Github} alt="Logo do gitghub" />
+    </Server>
+    <Server href="https://instagram.com/leuvargas" target="_new">
+      <img src={Instagram} alt="Logo do instagram" />
+    </Server>
+    <Server href="https://leonardovargas.me" target="_new">
+      <img src={Rocket} alt="Rocket icon" />
+    </Server>
+    <Server
+      href="https://www.linkedin.com/in/leonardo-luis-de-vargas/"
+      target="_new"
+      mentions={10}
+    >
+      <img src={Linkedin} alt="Logo do linkedin" />
+    </Server>
+    <Server href="https://open.spotify.com/user/leo_vargas" target="_new">
+      <img src={Spotify} alt="Logo do spotify" />
+    </Server>
+
     <Server className="active" />
     <Server />
     <Server />
-    <Server hasNotifications />
-    <Server />
-    <Server />
-    <Server mentions={10} />
   </Container>
 );
 
--- src/components/ServerList/styles.ts
+++ src/components/ServerList/styles.ts
@@ -12,10 +12,10 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  
+
   background: var(--tertiary);
   padding: 11px 0;
-  
+
   max-height: 100vh;
   overflow-y: scroll;
 
@@ -32,24 +32,25 @@ export const Separator = styled.div`
   margin-bottom: 8px;
 `;
 
-export const Server = styled.div<IServerProps>`
+export const Server = styled.a<IServerProps>`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-shrink: 0;
-  
+
   width: 48px;
   height: 48px;
   border-radius: 50%;
   margin-bottom: 8px;
-  background: ${props => props.isHome ? 'var(--rocketseat)' : 'var(--primary)' };
+  background: ${props => (props.isHome ? 'var(--primary)' : 'transparent')};
 
   transition-duration: .4s;
 
   &.active, &:hover, img:hover {
     border-radius: 20%;
+    text-decoration: none;
     cursor: pointer;
-    background: ${props => props.isHome ? 'var(--rocketseat)' : 'var(--discord)' };
+    background: ${props => (props.isHome ? 'var(--discord)' : 'transparent')};
   }
 
   position: relative;
@@ -85,7 +86,7 @@ export const Server = styled.div<IServerProps>`
     height: 16px;
 
     padding: 2px 3px;
-    
+
     position: absolute;
     bottom: -4px;
     right: -4px;
@@ -98,7 +99,8 @@ export const Server = styled.div<IServerProps>`
     font-weight: bold;
     color: #FFF;
 
-    content: '${props => (props.mentions && props.mentions)}';
-    display: ${props => (props.mentions && props.mentions > 0 ? 'inline': 'none')};
+    content: '${props => props.mentions && props.mentions}';
+    display: ${props =>
+      props.mentions && props.mentions > 0 ? 'inline' : 'none'};
   }
 `;
